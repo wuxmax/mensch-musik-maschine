8f2841908b887947335d3a6890441b393b11c0ae
fmaschhur <f.maschhur@live.de>
2022-05-12 14:37:30 +0200

    test_data changes

--- raspi/test.py
+++ raspi/test.py
@@ -8,14 +8,14 @@ from utils import load_config
 
 CONFIG_FILE = "config.yml"
 
-test_data0 = np.array([0, 0, 0, 0, 0]).reshape(1,-1)
-test_data1 = np.array([100, 0, 0, 0, 0]).reshape(1,-1)
-test_data2 = np.array([0, 100, 0, 0, 0]).reshape(1,-1)
-test_data3 = np.array([0, 0, 100, 0, 0]).reshape(1,-1)
-test_data4 = np.array([0, 0, 0, 100, 0]).reshape(1,-1)
-test_data5 = np.array([0, 0, 0, 0, 100]).reshape(1,-1)
-test_data6 = np.array([100, 0, 0, 0, 0]).reshape(1,-1)
-test_data7 = np.array([100, 100, 0, 0, 0]).reshape(1,-1)
+test_data0 = np.array([[0, 0, 0, 0, 0, 0, 0, 0], [0, 0, 0, 0, 0, 0, 0, 0]])
+test_data1 = np.array([[100, 0, 0, 0, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data2 = np.array([[100, 0, 0, 0, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data3 = np.array([[0, 0, 0, 0, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data4 = np.array([[0, 100, 0, 0, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data5 = np.array([[0, 0, 100, 0, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data6 = np.array([[0, 0, 0, 100, 0, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
+test_data7 = np.array([[0, 0, 0, 0, 100, 0, 0, 0], [700, 0, 0, 0, 700, 0, 0, 0]])
 
 
 @click.command()
@@ -39,7 +39,7 @@ def start_mmm(apply_log):
                     matpro.midi_player.reset()
                     break
     else:
-        test_data = [test_data6, test_data7]
+        test_data = [test_data0, test_data1, test_data2, test_data3, test_data4, test_data5, test_data6, test_data7]
         try:
             for value_matrix in test_data * 100:
                 print(value_matrix)
