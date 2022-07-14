5f949930819f42804a9ac2fbae4d9cebb787387d
reinhard <max.reinhard@dai-labor.de>
2022-07-14 19:02:36 +0200

    binary write working

--- raspi/pd_connector.py
+++ raspi/pd_connector.py
@@ -1,4 +1,5 @@
 import socket
+from time import sleep
 
 from utils import load_config
 
@@ -11,10 +12,24 @@ class PDConnector:
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         self.socket.connect(('localhost', 2342))
 
-    def send(self):
-        message = "hallo"
-        self.socket.send(message.encode('utf-8'))
+    def send(self, msg: str = ""):
+        fudi_msg = msg + ' ;'  # pure data uses the FUDI protocol (messages terminate with ';')
+        self.socket.send(fudi_msg.encode('ascii'))
+
+    def send_ints(self, ints: list[int]):
+        try:
+            assert all(int_ in range(0, 128) for int_ in ints)
+        except AssertionError:
+            print(f"Number must be in ASCII range (0, 128)!")
+
+        msg = " ".join(chr(int_) for int_ in ints)
+        self.send(msg)
 
 if __name__ == "__main__":
     connector = PDConnector(config)
-    connector.send()
+    
+    while True:
+        connector.send_ints([1, 5])
+        # connector.send("python")
+        print("sent")
+        sleep(1)
