6707b39cea70e2b287e4164e707252d09d902306
reinhard <max.reinhard@dai-labor.de>
2022-07-28 13:18:32 +0200

    pure data stuff

--- raspi/pd_connector.py
+++ raspi/pd_connector.py
@@ -3,33 +3,36 @@ from time import sleep
 
 from utils import load_config
 
-config = load_config()['pd_connector']
-
-
 class PDConnector:
     def __init__(self, config: dict) -> None:
-        # self.socket_port = config['socket_port']
         self.socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.socket.connect(('localhost', 2342))
+        self.socket.connect(('localhost', config['socket_port']))
+
+    # def send(self, msg: str = ""):
+    #     fudi_msg = msg + ' ;'  # pure data uses the FUDI protocol (messages terminate with ';')
+    #     self.socket.send(fudi_msg.encode('ascii'))
 
     def send(self, msg: str = ""):
-        fudi_msg = msg + ' ;'  # pure data uses the FUDI protocol (messages terminate with ';')
-        self.socket.send(fudi_msg.encode('ascii'))
+        self.socket.send(msg.encode('ascii'))
+
+    def send_int(self, int_: int):
+        try:
+            assert int_ in range(0, 128)
+        except AssertionError:
+            print(f"Number must be in ASCII range (0, 128), but is: {int_}")
+
+        self.send(chr(int_))
 
     def send_ints(self, ints: list[int]):
-        try:
-            assert all(int_ in range(0, 128) for int_ in ints)
-        except AssertionError:
-            print(f"Number must be in ASCII range (0, 128)!")
-
-        msg = " ".join(chr(int_) for int_ in ints)
-        self.send(msg)
+        for int_ in ints:
+            self.send_int(int_)
 
 if __name__ == "__main__":
+    config = load_config()['pd_connector']
     connector = PDConnector(config)
     
     while True:
         connector.send_ints([1, 5])
-        # connector.send("python")
         print("sent")
         sleep(1)
+        
