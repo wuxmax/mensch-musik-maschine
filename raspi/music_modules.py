e2e1765a16a434d068f0ed5de61de7783bdcd652
clemens.kuske <kuske@vincitos.com>
2022-08-01 11:49:35 +0200

    created mini sequencer

--- raspi/music_modules.py
+++ raspi/music_modules.py
@@ -76,10 +76,36 @@ class Sequencer(MusicModule):
                         channel=self.midi_channel,
                         note=self.midi_note,
                         velocity=MidiNoteEvent.value_range[-1],
-                        duration = self.note_duration)
+                        duration=self.note_duration)
                         ]
             self.beats += 1
         
+        # print(f"sequencer: {return_list}")
+        return return_list
+
+class MiniSequencer(MusicModule):
+    def __init__(self, setup, sound):
+        super().__init__(setup)
+        self.beat_duration = 60 / sound['bpm']
+        self.note_duration = sound['note_duration']
+        self.midi_note = sound['midi_note']
+        self.start_time = datetime.datetime.now()
+        self.beats = 0
+
+    def module_process(self, matrix: np.ndarray):
+        return_list = []
+        # check for correct time
+        if (datetime.datetime.now() - self.start_time).total_seconds() / self.beat_duration > self.beat_duration:
+            # check for correct value
+            if matrix[0][0] != 0:
+                return_list = [
+                    MidiNoteEvent(
+                        channel=self.midi_channel,
+                        note=self.midi_note,
+                        velocity=MidiNoteEvent.value_range[-1],
+                        duration=self.note_duration)
+                        ]
+
         # print(f"sequencer: {return_list}")
         return return_list
         
@@ -94,5 +120,3 @@ class SimpleControl(MusicModule):
             channel=self.midi_channel, 
             control=self.control, 
             value=np.mean(matrix))
-
-
