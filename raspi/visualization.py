d2919cedb48430b3c6105f47c801e97fc3f9b128
clemens.kuske <kuske@vincitos.com>
2022-08-11 21:08:54 +0200

    debug

--- raspi/visualization.py
+++ raspi/visualization.py
@@ -15,39 +15,50 @@ class Interface():
         self.module_name_spacer: int = max([len(module.name) for module in self.modules])
 
     def render(self) -> None:
+        return self.quick_and_dirty_for_testing_values()
         activations, names = self.get_activations_and_names()
         vertical, horizontal = self.shape
-        # assert activations.shape[1] == horizontal
-        # assert len(names[0]) == horizontal
+        assert activations.shape[1] == horizontal
+        assert len(names[0]) == horizontal
         
         table = []
-        table.append([f"{a:.0f}" for a in activations])
-        # for i in range(vertical):
+        for i in range(vertical):
             # add names if any
-            # if any(elem is not None for elem in names[i]):
-                # table.append([name if name else "-" * self.module_name_spacer for name in names[i]])
+            if any(elem is not None for elem in names[i]):
+                table.append([name if name else "-" * self.module_name_spacer for name in names[i]])
             
             # add activations
-            # table.append([f"{a:.0f}" for a in activations])
+            table.append([f"{a:.0f}" for a in activations[i]])
 
         # print(tabulate(table), flush=True)
         # sys.stdout.flush()
         # os.system('cls' if os.name == 'nt' else 'clear')
-        s = ' '.join(str(f"{v:.0f}") for v in activations)
-        sys.stdout.write("\r{0}".format(s))
+        sys.stdout.write(tabulate(table))
         sys.stdout.flush()
 
-    def get_activations_and_names(self) -> np.ndarray:
+    def quick_and_dirty_for_testing_values(self):
         activations = np.zeros(self.shape[0] * self.shape[1])
-        names = [[None for i in range(self.shape[1])] for i in range(self.shape[0])]
-        values = 0
+        counter = 0
         for module in self.modules:
             # get activation values for sensors in module
             module_activations = module.get_values()
             for i in range(module_activations.shape[0]):
                 for j in range(module_activations.shape[1]):
-                    activations[values] = module_activations[i][j]
-                    values += 1
+                    activations[counter] = module_activations[i][j]
+                    counter += 1
+        s = ' '.join(str(f"{v:.0f}") for v in activations)
+        sys.stdout.write("\r{0}".format(s))
+        sys.stdout.flush()
+
+    def get_activations_and_names(self) -> np.ndarray:
+        activations = np.zeros(self.shape)
+        names = [[None for i in range(self.shape[1])] for i in range(self.shape[0])]
+        for module in self.modules:
+            # get activation values for sensors in module
+            module_activations = module.get_values()
+            for i in range(module_activations.shape[0]):
+                for j in range(module_activations.shape[1]):
+                    activations[i+module.top][j+module.left] = module_activations[i][j]
             # save name of module
             names[module.top][module.left] = module.name
         return activations, names
