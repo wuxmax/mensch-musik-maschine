003ecfda0f4a25ca7751f768f53b460e57a1da35
clemens.kuske <kuske@vincitos.com>
2022-08-11 20:43:14 +0200

    debug

--- raspi/visualization.py
+++ raspi/visualization.py
@@ -32,7 +32,7 @@ class Interface():
         # print(tabulate(table), flush=True)
         # sys.stdout.flush()
         # os.system('cls' if os.name == 'nt' else 'clear')
-        # print(tabulate(table))
+        sys.stdout.write(tabulate(table))
 
 
     def get_activations_and_names(self) -> np.ndarray:
