d830eb04530ad065ca2ce1c8d2a74779b10cb11b
clemens.kuske <kuske@vincitos.com>
2022-10-17 16:28:07 +0200

    debug

--- raspi/music_modules/decayer.py
+++ raspi/music_modules/decayer.py
@@ -14,16 +14,17 @@ class Decayer(MusicModule):
         self.decay_delay = sound['decay_delay']
 
         self.activation = 0
-        self.activation_timestamp = None
+        self.activation_timestamp = 0
         self.info = ''
+        self.old_matrix = []
 
     def get_info(self) -> str:
         return self.info
 
     def module_process(self, matrix: np.ndarray):
-        if np.any(matrix):
+        if self.old_matrix and np.any(matrix - self.old_matrix):
             self.activation_timestamp = time.time()
-        
+        self.old_matrix = matrix
         decay_time = time.time() - self.activation_timestamp - self.decay_delay
         signal_loss = np.clip(decay_time * self.decay_rate, 0, 1)
 
