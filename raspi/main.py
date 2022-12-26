fd053e9ad202047399fe39d3a70dbb637c1d238e
clemens.kuske <kuske@vincitos.com>
2022-12-26 20:44:07 +0100

    angular frontend

--- raspi/main.py
+++ raspi/main.py
@@ -128,7 +128,7 @@ async def sensor_values(websocket: WebSocket):
         print('sending data')
         data = await websocket.receive_text()
         print(data)
-        await websocket.send_text('simple_values')
+        await websocket.send_json({'asd': 'simple_values'})
         print('data send')
 
 
