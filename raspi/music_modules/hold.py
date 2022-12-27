498b000d96ea5abeb3ee83eb576f40a717937638
clemens.kuske <kuske@vincitos.com>
2022-12-27 01:14:34 +0100

    angular frontend

--- raspi/music_modules/hold.py
+++ raspi/music_modules/hold.py
@@ -22,14 +22,16 @@ class Hold(MusicModule):
     def module_process(self, matrix: np.ndarray):
         self.history.append(matrix)
         if time.time() - self.timer > self.time_step_size:
+            print('----')
             events = []
             self.timer = time.time()
             for i, array in enumerate(self.activation):
                 for j, _ in enumerate(array):
                     self.activation[i][j] = self.calculate_activation(self.activation[i][j], np.array(self.history)[:, i, j])
+                    print(2 + (i * 2) * (j + 1))
                     events.append(MidiControlEvent(
                         channel=self.midi_channel,
-                        control=2 + (i * 2) * (j + 1),
+                        control=2 + (i * 2) * (j + 1),  # control_values 4, 5, 6, 7
                         value=self.activation[i][j]))
             self.set_info(np.array(self.activation).flatten())
             return events
