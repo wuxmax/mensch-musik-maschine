a9c6c13d7ecbff2fa369fcc6eb20f8a7d3282254
clemens.kuske <kuske@vincitos.com>
2022-12-27 15:45:19 +0100

    add scene changer

---
+++ raspi/music_modules/scene_changer.py
@@ -0,0 +1,25 @@
+import time
+from collections import deque
+
+import numpy as np
+
+from sound_events import MidiControlEvent
+from .base import MusicModule
+from module_logger import ModuleLogger
+
+
+class SceneChanger(MusicModule):
+    def __init__(self, setup, sound, module_logger: ModuleLogger):
+        super().__init__(setup, module_logger)
+        self.scene_time = sound['scene_time']
+        self.control = sound['control']
+        self.time_step_size = sound['time_step_size']
+        self.timer = time.time()
+
+
+    def module_process(self, _):
+        if time.time() - self.timer > self.time_step_size:
+            return MidiControlEvent(
+                channel=self.midi_channel,
+                control=self.control,
+                value=127)
